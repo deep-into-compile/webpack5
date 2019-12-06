4158153d3c93ccc50ffe03eb828fc521d7167ba9
chenning <chenning@sensorsdata.cn>
2019-12-06 14:41:45 +0800

    copy webpack5.0.0-beta7

---
+++ test/TestCasesCachePack.test.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const { describeCases } = require("./TestCases.template");
+
+describe("TestCases", () => {
+	describeCases({
+		name: "cache pack",
+		cache: {
+			type: "filesystem",
+			managedPaths: [path.resolve(__dirname, "../node_modules")]
+		},
+		optimization: {
+			innerGraph: true,
+			usedExports: true
+		}
+	});
+});
